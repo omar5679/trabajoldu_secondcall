e958202e11076186e799a87970c840cce93c0b4c
AJRR01 <0619999429@uma.es>
2023-04-18 20:34:39 +0200

    Añadido icono de usuario a la cabecera

--- js/SesionCuenta.js
+++ js/SesionCuenta.js
@@ -4,5 +4,14 @@ function cambioCabeceraSesion() {
     if (cuenta.sesionActiva) {
         elemento.children[0].innerText = "¡Bienvenido!";
         elemento.href = "/OpcionesCuenta.html"
+        const icono = document.createElement("svg");
+        icono.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        icono.setAttribute("width", "16");
+        icono.setAttribute("height", "16");
+        icono.setAttribute("fill", "currentColor");
+        icono.setAttribute("class", "bi bi-person-circle");
+        icono.setAttribute("viewBox", "0 0 16 16");
+        icono.innerHTML = '<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>\n<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>'
+        elemento.insertBefore(icono, elemento.children[0]);
     }
 }
