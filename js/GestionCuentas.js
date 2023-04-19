124a80d1e056c887fbbf910cf4752de963683c47
NachoK66 <nachok662000@gmail.com>
2023-04-19 10:03:27 +0200

    Enlaces corregidos y organizacion

--- js/GestionCuentas.js
+++ js/GestionCuentas.js
@@ -12,7 +12,7 @@ function crearCuenta() {
     if (password === document.getElementById("contraseña-repetida").value) {
         const cuenta = new Cuenta(email, password);
         sessionStorage.setItem("cuenta", JSON.stringify(cuenta));
-        window.location.href="../PaginaPrincipal.html";
+        window.location.href="/PaginaPrincipal.html";
         alert("Cuenta creada. ¡Bienvenido!");
     } else {
         alert("Las contraseñas introducidas no coinciden entre sí. Inténtelo de nuevo.")
@@ -26,7 +26,7 @@ function iniciarSesion() {
     if (cuenta != null && email === cuenta.email && password === cuenta.password) {
         cuenta.sesionActiva = true;
         sessionStorage.setItem("cuenta", JSON.stringify(cuenta));
-        window.location.href="../PaginaPrincipal.html";
+        window.location.href="/PaginaPrincipal.html";
         alert("¡Bienvenido de vuelta!");
     } else {
         alert("Email o contraseña incorrectos. Inténtelo de nuevo.");
@@ -37,6 +37,6 @@ function cerrarSesion() {
     const cuenta = JSON.parse(sessionStorage.getItem("cuenta"));
     cuenta.sesionActiva = false;
     sessionStorage.setItem("cuenta", JSON.stringify(cuenta));
-    window.location.href="./PaginaPrincipal.html";
+    window.location.href="/PaginaPrincipal.html";
     alert("Sesión cerrada.");
 }
