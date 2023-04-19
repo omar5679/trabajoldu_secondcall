d2496de3c52e877c9d4b04d6fa2dd4bc2e07db52
NachoK66 <nachok662000@gmail.com>
2023-04-19 22:35:41 +0200

    Añadida barra de porcentaje y algunas mejoras visuales

--- js/PaginasVisitadas.js
+++ js/PaginasVisitadas.js
@@ -13,45 +13,91 @@ let diccionario = {
 	'Autoevaluación en C' : "EjemploAutoevaluacion"
   };
 
+const tutosC = 12;
+let visitedCPages;
 
+//Al cargar la pagina, si estamos en Cuenta, recoge el componente perfil y lo actualiza si es necesario
 document.addEventListener("DOMContentLoaded", function() {
-	const profile = document.getElementById("profile");
-
-	function showLast() {
-		lastPage = sessionStorage.getItem('lastPage');
-	  console.log("lastPage es: " + lastPage);
-	  if (lastPage != null) {
-		const newItem = document.createElement('h1');
-		const linktext = document.createElement('a');
-		linktext.setAttribute('href', '/Tutoriales/C/' + diccionario[lastPage] + ".html");
-		console.log("El valor de " + lastPage + " en diccionario es " + diccionario[lastPage]);
-		linktext.appendChild(document.createTextNode(lastPage));
-		newItem.appendChild(document.createTextNode("Vuelve a: "));
-		newItem.appendChild(linktext);
-		profile.insertBefore(newItem, profile.children[1]);
-	  } else {
-		console.log("lastPage es nulo");
-	  }
-	}
-
-	showLast();
-  });
-/*
-  // Función para devolver el valor de la variable global
-  function showLast() {
-	console.log("lastPage es: " + sessionStorage.getItem('lastPage'));
-	console.log("profile es: " + profile);
-	if (sessionStorage.getItem('lastPage') != null)
+	if (window.location.pathname == '/Cuenta/Cuenta.html')
 	{
-		const newItem = document.createElement('h1');
-		newItem.appendChild(document.createTextNode("Vuelve a: " + sessionStorage.getItem('lastPage')));
+		const profile = document.getElementById("profile");
 
-		profile.appendChild(newItem);
+		showLast();
+		showPercentage();
 	} else
+		console.log("Estoy en ubicacion: " + window.location.pathname);
+});
+
+function showLast() {
+	lastPage = sessionStorage.getItem('lastPage');
+  console.log("lastPage es: " + lastPage);
+
+  if (lastPage != null) {
+	const newItem = document.createElement('h1');
+	const linktext = document.createElement('a');
+	linktext.setAttribute('href', '/Tutoriales/C/' + diccionario[lastPage] + ".html");
+	console.log("El valor de " + lastPage + " en diccionario es " + diccionario[lastPage]);
+	linktext.appendChild(document.createTextNode(lastPage));
+	newItem.appendChild(document.createTextNode("Vuelve a: "));
+	newItem.appendChild(linktext);
+	profile.insertBefore(newItem, profile.children[1]);
+  } else {
+	console.log("lastPage es nulo");
+  }
+}
+
+function showPercentage(){
+	//if(!visitedCPages)
+	//	sessionStorage.setItem('visitedCPages', JSON.stringify([]));
+	visitedCPages = JSON.parse(sessionStorage.getItem('visitedCPages'));
+	let barraProgreso = document.getElementById('progressBar');
+	let percentageNumber = document.createElement('span');
+	if(!barraProgreso && visitedCPages.length > 0)
 	{
-		console.log("lastPage es nulo");
+		const newItem = document.createElement('a');
+		newItem.innerText = "Progreso actual en C";
+
+		barraProgreso = document.createElement('progress');
+		barraProgreso.setAttribute('id', 'progressBar');
+		barraProgreso.setAttribute('value', '0');
+		barraProgreso.setAttribute('max', '100');
+		barraProgreso.style.marginLeft = "20px";
+
+		percentageNumber.innerText = "0%";
+		percentageNumber.style.marginLeft = "20px";
+
+		newItem.appendChild(barraProgreso);
+		newItem.appendChild(percentageNumber);
+
+		console.log("Numero hijos de profile: " + profile.children.length);
+		profile.insertBefore(newItem, profile.children[profile.children.length - 3]); //parece que el boton es el antepenultimo o algo
+		profile.insertBefore(document.createElement('br'), profile.children[profile.children.length - 3]);
 	}
-  }
-*/
-  function showPercentage(){
-  }
+	barraProgreso.setAttribute('value', visitedCPages.length / tutosC * 100);
+	percentageNumber.innerText = (visitedCPages.length / tutosC * 100).toFixed(1) + "%";
+	console.log("Llevas un " + visitedCPages.length / tutosC * 100 + "% completo de la lección de C. Lecciones: " + visitedCPages.length);
+}
+
+function addPage(val){
+	//let visitedCPages = JSON.parse(localStorage.getItem('visitedCPages'));
+	//console.log("Numero elementos en array: " + visitedCPages.length);
+	if(!visitedCPages)
+	{
+		console.log("ENTRO EN NULL");
+		visitedCPages = [];
+		visitedCPages.push(val);
+		sessionStorage.setItem('visitedCPages', JSON.stringify(visitedCPages));
+		console.log("Añadido " + val + " a lista de C. Num elementos: " + visitedCPages.length);
+	}
+	else
+	{
+		visitedCPages = JSON.parse(sessionStorage.getItem('visitedCPages'));
+		console.log("Array es " + visitedCPages);
+		if(!visitedCPages.includes(val))
+		{
+			visitedCPages.push(val);
+			sessionStorage.setItem('visitedCPages', JSON.stringify(visitedCPages));
+			console.log("Añadido " + val + " a lista de C. Num elementos: " + visitedCPages.length);
+		}
+	}
+}
